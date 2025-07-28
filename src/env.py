6e9eb21f6c472fdc009fd7c6fb58f53b98f12146
J.P <jiangp@fusionskye.com>
2025-07-28 10:42:02 +0800

    feat(http): 增加HTTP响应行和头字段大小限制配置

--- src/env.py
+++ src/env.py
@@ -1,19 +1,3 @@
-#  RSS to Telegram Bot
-#  Copyright (C) 2020-2024  Rongrong <i@rong.moe>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as
-#  published by the Free Software Foundation, either version 3 of the
-#  License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 from __future__ import annotations
 from typing import Optional, Union
 from typing_extensions import Final
@@ -298,6 +282,9 @@ VERIFY_TLS: Final = __bool_parser(os.environ.get('VERIFY_TLS'), default_value=Tr
 HTTP_TIMEOUT: Final = int(os.environ.get('HTTP_TIMEOUT') or 12)
 HTTP_CONCURRENCY: Final = int(os.environ.get('HTTP_CONCURRENCY') or 1024)
 HTTP_CONCURRENCY_PER_HOST: Final = int(os.environ.get('HTTP_CONCURRENCY_PER_HOST') or 16)
+
+HTTP_MAX_LINE_SIZE: Final = int(os.environ.get('HTTP_MAX_LINE_SIZE') or 16384)
+HTTP_MAX_FIELD_SIZE: Final = int(os.environ.get('HTTP_MAX_FIELD_SIZE') or 16384)
 
 # ----- img relay server config -----
 _img_relay_server = os.environ.get('IMG_RELAY_SERVER') or 'https://rsstt-img-relay.rongrong.workers.dev/'
